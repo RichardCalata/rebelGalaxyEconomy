c125550e3e038d6912520fb267c6415c25e5ed62
richardcalata <calatar6@gmail.com>
2019-08-21 12:49:50 -0500

    corrected some js rename some variables

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
+"use strict";
 
-function renderCommodity(commodites) {
-    var html = '<tr class="commodities">';
-    html += '<td>' + commodites.id + '</td>';
-    html += '<td>' + commodites.name + '</td>';
-    html += '<td>' + commodites.soldAt + '</td>';
-    html += '<td>' + commodites.buyAt + '</td>';
+function renderGood(good) {
+    var html = '<tr class="good">';
+    html += '<td>' + good.id + '</td>';
+    html += '<td>' + good.name + '</td>';
+    html += '<td>' + good.soldAt + '</td>';
+    html += '<td>' + good.buyAt + '</td>';
 
     return html
 }
 
 
-function renderCommodity(commodities){
+function renderGoods(goods){
     var html = '';
-    for(var i = commodities.length -1; i>= 0; i--){
-        html += renderCommodity(commodities[i]);
+    for(var i = goods.length -1; i>= 0; i--){
+        html += renderGood(goods[i]);
     }
     return html
 }
 
-var commodities = [
-    {id: 1, name:'Alien Artifacts', soldAt:'', buyAt:''}
-]
+var goods = [
+    {id: 1, name:'Alien Artifacts', soldAt:'', buyAt:''},
+    {id: 2, name:'Alien Fossils', soldAt:'', buyAt:''},
+    {id: 3, name:'Alien Specimens', soldAt:'', buyAt:''},
+    {id: 4, name:'Anomalies', soldAt:'', buyAt:''},
+    {id: 5, name:'Antimatter Speck', soldAt:'', buyAt:''},
+    {id: 6, name:'Ceramics', soldAt:'', buyAt:''},
+    {id: 7, name:'Components', soldAt:'', buyAt:''}
+];
+
+var tbody = document.querySelector('#goods');
+
+tbody.innerHTML = renderGoods(goods);
